4678d3f2c714302c12ba346340c78379230b7f4f
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-12 11:19:38 +0530

    added UserController.java

--- myGarageHub/src/main/java/com/myGarageHub/controller/UserController.java
+++ myGarageHub/src/main/java/com/myGarageHub/controller/UserController.java
@@ -3,9 +3,13 @@ package com.myGarageHub.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.myGarageHub.service.UserService;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-	
+private final UserService userService;
+
+
 }
