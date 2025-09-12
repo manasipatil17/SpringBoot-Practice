ebfc8ba26bdbd0f8f59cd8f866c93b97641dbe19
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-12 11:29:07 +0530

    added UserController code

--- myGarageHub/src/main/java/com/myGarageHub/controller/UserController.java
+++ myGarageHub/src/main/java/com/myGarageHub/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.myGarageHub.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.myGarageHub.entity.User;
 import com.myGarageHub.service.UserService;
 
 @RestController
@@ -11,5 +18,22 @@ public class UserController {
 
 private final UserService userService;
 
+public UserController(UserService userService) {
+	this.userService=userService;
+}
 
+@PostMapping("/createUser")
+public User createUser(@RequestBody User user) {
+	return userService.createUser(user);
+}
+
+@GetMapping("/getAllUsers")
+public List<User> getAllUsers() {
+    return userService.getAllUsers();
+}
+
+@GetMapping("/getUserById/{id}")
+public User getUserById(@PathVariable Long id) {
+	return userService.getUserById(id);
+}
 }
