e73f93d74f10a8989e3f1bb5bd73b54ac810fba5
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-12 11:09:39 +0530

    added UserService code

--- myGarageHub/src/main/java/com/myGarageHub/service/UserService.java
+++ myGarageHub/src/main/java/com/myGarageHub/service/UserService.java
@@ -1,8 +1,32 @@
 package com.myGarageHub.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.myGarageHub.entity.User;
+import com.myGarageHub.repository.UserRepository;
 
 @Service
 public class UserService {
 
+	private final UserRepository userRepo;
+
+    public UserService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public User createUser(User user) {
+        return userRepo.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepo.findById(id).orElse(null);
+    }
 }
+
+
