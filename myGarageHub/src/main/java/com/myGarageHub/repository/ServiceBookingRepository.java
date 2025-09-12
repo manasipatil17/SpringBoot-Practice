513128bd98f4fef99ce2ff4ed02314c1dad2e5d3
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-12 11:05:20 +0530

    added ServiceBookingRepository code

--- myGarageHub/src/main/java/com/myGarageHub/repository/ServiceBookingRepository.java
+++ myGarageHub/src/main/java/com/myGarageHub/repository/ServiceBookingRepository.java
@@ -1,5 +1,9 @@
 package com.myGarageHub.repository;
 
-public interface ServiceBookingRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myGarageHub.entity.ServiceBooking;
+
+public interface ServiceBookingRepository extends JpaRepository<ServiceBooking, Long>{
 
 }
