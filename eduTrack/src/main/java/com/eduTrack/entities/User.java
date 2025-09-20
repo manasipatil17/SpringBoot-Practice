a9acc240f58938439f10d0c06e7f5ba2e9896194
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-20 11:41:21 +0530

    added User.class

---
+++ eduTrack/src/main/java/com/eduTrack/entities/User.java
@@ -0,0 +1,72 @@
+package com.eduTrack.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	String username;
+	String password;
+	String firstname;
+	String lastname;
+	String email;
+	String role;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(String username, String password, String firstname, String lastname, String email, String role) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.role = role;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + ", firstname=" + firstname + ", lastname="
+				+ lastname + ", email=" + email + ", role=" + role + "]";
+	}
+	
+	
+}
