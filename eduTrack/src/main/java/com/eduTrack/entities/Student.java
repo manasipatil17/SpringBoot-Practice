a9759b6f8ea0d2796e3b88bf11f995ae32929d79
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-20 11:35:01 +0530

    added Student.class

--- eduTrack/src/main/java/com/eduTrack/entities/Student.java
+++ eduTrack/src/main/java/com/eduTrack/entities/Student.java
@@ -13,9 +13,39 @@ public class Student {
 	private long id;
 	private String name;
 	private String email;
+	
+	
 	public Student() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+	public Student(long id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", email=" + email + "]";
 	}
 	
 	
